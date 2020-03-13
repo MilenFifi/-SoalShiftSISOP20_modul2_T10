7bc03d421bac182167641fb24dd6e23397353acf
Milenia Ulwan Zafira <55182134+MilenFifi@users.noreply.github.com>
2020-03-13 15:24:18 +0700

    Update killer.sh

--- soal2/killer.sh
+++ soal2/killer.sh
@@ -5,5 +5,4 @@ do
     ps aux | grep s3 | awk '$7 ~/?/ { printf "%s", $2; }' | (read msg; kill -9 $msg;);
   elif [[ $i == "-b" ]]; then
     ps aux | grep s3 | awk '$7 ~/?/ { printf "%s", $2; }' | (read msg; kill -2 $msg;);
-exit 0
-fi
+  fi
