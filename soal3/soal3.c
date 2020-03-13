4f55c50e6bd9e59771ec526de4832353c947e8eb
Milenia Ulwan Zafira <55182134+MilenFifi@users.noreply.github.com>
2020-03-13 18:05:38 +0700

    Update soal3.c

--- soal3/soal3.c
+++ soal3/soal3.c
@@ -4,22 +4,16 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
-#include <time.h>
 #include <wait.h>
 
 int main() {
 
   pid_t child_id = fork();
   if (child_id == 0) {
-    char *argv[] = {"mkdir", "-p", "/home/milenfifi/modul2/indomie", NULL};
+    char *argv[] = {"mkdir", "-p", "/home/milenfifi/modul2/indomie", "/home/milenfifi/modul2/sedaap", NULL};
     execv("/bin/mkdir", argv);
   }
   while(wait(NULL) != child_id);
   sleep(5);
 
-  child_id = fork();
-  if (child_id == 0) {
-    char *argv[] = {"mkdir", "-p", "/home/milenfifi/modul2/sedaap", NULL};
-    execv("/bin/mkdir", argv);
-  }
   
